2d5280bcb6b913e44a9bbd6ca3fbda0ca03f99e3
Ichel Delgado <icheldmorales@gmail.com>
2024-08-19 21:46:49 -0500

    Adding QuickSort

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,31 +1,31 @@
 public class QuickSort {
-    public static void quickSort(int[] array, int low, int high) {
+    public static void quickSort(int[] inputArray, int low, int high) {
         if (low < high) {
-            int pi = partition(array, low, high);
+            int index = partition(inputArray, low, high);
 
-            quickSort(array, low, pi - 1);
-            quickSort(array, pi + 1, high);
+            quickSort(inputArray, low, index - 1);
+            quickSort(inputArray, index + 1, high);
         }
     }
 
-    private static int partition(int[] array, int low, int high) {
-        int pivot = array[high];
-        int i = (low - 1);
+    private static int partition(int[] inputArray, int low, int high) {
+        int pivot = inputArray[high];
+        int lowestIndex = (low - 1);
 
         for (int j = low; j < high; j++) {
-            if (array[j] <= pivot) {
-                i++;
+            if (inputArray[j] <= pivot) {
+                lowestIndex++;
 
-                int temp = array[i];
-                array[i] = array[j];
-                array[j] = temp;
+                int tempArray = inputArray[lowestIndex];
+                inputArray[lowestIndex] = inputArray[j];
+                inputArray[j] = tempArray;
             }
         }
 
-        int temp = array[i + 1];
-        array[i + 1] = array[high];
-        array[high] = temp;
+        int tempArray = inputArray[lowestIndex + 1];
+        inputArray[lowestIndex + 1] = inputArray[high];
+        inputArray[high] = tempArray;
 
-        return i + 1;
+        return lowestIndex + 1;
     }
 }
